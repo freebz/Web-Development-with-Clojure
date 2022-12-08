e7e32dea9fe3104ac75c6451fbb9e8a0cfd70930
fx <fx@P54M.mshome.net>
2022-12-08 11:39:38 +0900

    5. Picture Gallery

---
+++ ch05/picture-gallery/src/picture_gallery/models/schema.clj
@@ -0,0 +1,18 @@
+(ns picture-gallery.models.schema
+  (:require [picture-gallery.models.db :refer :all]
+            [clojure.java.jdbc :as sql]
+            [picture-gallery.models.db :as db]))
+
+(defn create-users-table []
+  (sql/with-connection db
+    (sql/create-table
+     :users
+     [:id "varchar(32) PRIMARY KEY"]
+     [:pass "varchar(100)"])))
+
+(defn create-images-table []
+  (sql/with-connection db
+    (sql/create-table
+     :images
+     [:userid "varchar(32)"]
+     [:name "varchar(100)"])))
